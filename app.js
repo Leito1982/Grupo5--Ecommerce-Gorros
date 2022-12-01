58919529642dbae6b2c457e1e27c4e7f0065bf37
Emilix22 <torresdragon@hotmail.com>
2022-12-01 09:20:33 -0300

    cree las rutas

--- app.js
+++ app.js
@@ -9,5 +9,21 @@ app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html")) 
 });
 
+app.get ('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html")) 
+});
+
+app.get ('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html")) 
+});
+
+app.get ('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html")) 
+});
+
+app.get ('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html")) 
+});
+
 app.listen (port, () => 
 console.log("Esta corriendo el puerto "+port));
