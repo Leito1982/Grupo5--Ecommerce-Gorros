575d6e8319605e5d3ecb48bff2c93d70ff9b97c4
Emilix22 <torresdragon@hotmail.com>
2023-01-05 10:32:29 -0300

    reestructuro home con tags ejs

--- dataBase/products.js
+++ dataBase/products.js
@@ -4,7 +4,68 @@ let products = [
         image: '/images/FER-1.jpg',
         name: 'FERRARI',
         price: '$4300'
+    },
+    {
+        id: 2,
+        image: '/images/ADI1.jpg',
+        name: 'ADIDAS',
+        price: '$4300'
+    },
+    {
+        id: 3,
+        image: '/images/DUF-1.jpg',
+        name: 'DUFF',
+        price: '$4300'
+    },
+    {
+        id: 4,
+        image: '/images/cor-1.jpg',
+        name: 'CORONA',
+        price: '$4300'
+    },
+    {
+        id: 5,
+        image: '/images/NY1.jpg',
+        name: 'NEW YORK',
+        price: '$4300'
+    },
+    {
+        id: 6,
+        image: '/images/HON-1.jpg',
+        name: 'HONDA',
+        price: '$4300'
+    },
+    {
+        id: 7,
+        image: '/images/KTM-1.jpg',
+        name: 'KTM',
+        price: '$4300'
+    },
+    {
+        id: 8,
+        image: '/images/PUM-1.jpg',
+        name: 'PUMA',
+        price: '$4300'
+    },
+    {
+        id: 9,
+        image: '/images/riv-1.jpg',
+        name: 'RIVER',
+        price: '$4300'
+    },
+    {
+        id: 10,
+        image: '/images/boc-1.jpg',
+        name: 'BOCA',
+        price: '$4300'
+    },
+    {
+        id: 11,
+        image: '/images/V46-1.jpg',
+        name: 'V46',
+        price: '$4300'
     }
+
 ]
 
 module.exports = products;
