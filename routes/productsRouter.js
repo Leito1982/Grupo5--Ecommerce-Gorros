a20f5918c6d4ac95c335647063bdb66c2032e1b1
Emilix22 <torresdragon@hotmail.com>
2023-02-02 16:57:57 -0300

    multer en carpeta middleware

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
+const upload = require('../middlewares/multer')
 
 const productsController = require('../controllers/productsController');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, uniqueSuffix + path.extname(file.originalname))
-    }
-  })
-  
-  const upload = multer({ storage: storage })
   
 
 router.get('/', productsController.index);  
