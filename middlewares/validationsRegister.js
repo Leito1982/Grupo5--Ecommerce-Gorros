3be194c083ecdc348f353b54992aea65436796d1
Emilix22 <torresdragon@hotmail.com>
2023-04-04 09:42:43 -0300

    se agrega validacion para extenciones de imagenes con express-validator

--- middlewares/validationsRegister.js
+++ middlewares/validationsRegister.js
@@ -1,4 +1,5 @@
 const { body } = require('express-validator');
+const path = require('path')
 
 const validationsRegister = [
   body('firstName').notEmpty()
@@ -10,67 +11,18 @@ const validationsRegister = [
   body('email').isEmail().withMessage('El email no es válido'),
   body('password').isLength({ min: 8}).withMessage('El password debe tener como mínimo 8 caracteres'),
   
+  body('image').custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = ['.jpg', '.png', '.gif', '.jpeg', '.webp'];
 
-// Aca tenemos que copiar la parte de codigo de "pruebas" para validar las extensiones de las imagenes
-// Cuando se corto la clase no lo termine de armar. 27/3
-
-
-
-  ]
+    if (file) {
+      let fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+      }
+    }
+  })
+]
 
   module.exports =validationsRegister;
 
-
-
-
-
-  // -------------------------------- Pruebas ----
-
-  // body('image1').custom((value, { req }) => {
-	// 	let file = req.files[0];
-	// 	let acceptedExtensions = ['.jpg', '.png', '.gif', '.jpeg', '.webp'];
-
-	// 	if (!file) {
-	// 		throw new Error('Tienes que subir una imagen');
-	// 	} else {
-	// 		let fileExtension = path.extname(file.originalname);
-	// 		if (!acceptedExtensions.includes(fileExtension)) {
-	// 			throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
-	// 		}
-	// 	}
-
-
-
-  // app.use(expressValidator({
-  //   customValidators: {
-  //       isImage: function(value, filename) {
-    
-  //           var extension = (path.extname(filename)).toLowerCase();
-  //           switch (extension) {
-  //               case '.jpg':
-  //                   return '.jpg';
-  //               case '.jpeg':
-  //                   return '.jpeg';
-  //               case  '.png':
-  //                   return '.png';
-  //               default:
-  //                   return false;
-  //           }
-  //       }
-  //   }}));
-
-
-
-
-
-  //             // Array of allowed files
-// const array_of_allowed_files = ['png', 'jpeg', 'jpg', 'gif'];
-
-// // Get the extension of the uploaded file
-// const file_extension = image.originalname.slice(
-//     ((image.originalname.lastIndexOf('.') - 1) >>> 0) + 2
-// );
-
-// // Check if the uploaded file is allowed
-// if (!array_of_allowed_files.includes(file_extension)) {
-//   throw Error('Invalid file');
