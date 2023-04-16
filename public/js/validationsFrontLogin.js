eab3ba49c50702c32abc406be7352ff365112f2f
Emilix22 <torresdragon@hotmail.com>
2023-04-16 19:31:07 -0300

    termino validaciones front del formulario login

--- public/js/validationsFrontLogin.js
+++ public/js/validationsFrontLogin.js
@@ -1,53 +1,48 @@
 window.onload = function () {
 
-  let errores = [];
-  const email = document.querySelector('#email');
-  const password = document.querySelector('#password');
-  const btnSubmit = document.querySelector(".button")
-  let ulErrores = document.querySelector(".errores ul");
-  ulErrores.style.color = 'red';
-
-  //validaciones email
-  email.addEventListener('blur', (e) => {
-    errores = [];
-    ulErrores.innerHTML = '';
-    emailRegex = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
-
-    if(email.value == ""){
-      errores.push("El campo email es obligatorio");
-      email.style.backgroundColor = 'pink';
-    }else if (emailRegex.test(e.target.value)) {
-      email.style.backgroundColor = 'white';
-    }else {
-      email.style.backgroundColor = 'pink';
-      errores.push("El formato del email no es válido");
+  const formulario = document.querySelector('#formulario');
+  const inputs = document.querySelectorAll('#formulario .form-camp input');
+  const expresiones = {
+    email: /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i,
+    password: /^.{8,30}$/
     }
 
-    errores.forEach(error => {
-    ulErrores.innerHTML += `<li>${error}</li>`
-    })   
-  })
-
-  email.addEventListener('input', (e) => {
-    
-    if (email.value != "") {
-      email.style.backgroundColor = 'white';
-      ulErrores.innerHTML = '';
-    }else{
-      email.style.backgroundColor = 'pink';  
+  const validarFormulario = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        if (expresiones.email.test(e.target.value)) {
+          e.target.style.backgroundColor = 'white';
+          document.querySelector('#input-error-email').classList.remove('input-error-email-active')
+          document.querySelector('#input-error-email').classList.add('input-error-email')
+        }else{
+          e.target.style.backgroundColor = 'pink';
+          document.querySelector('#input-error-email').classList.add('input-error-email-active')
+          document.querySelector('#input-error-email').classList.remove('input-error-email')
+        }
+      break;
+      case 'password':
+        if (expresiones.password.test(e.target.value)) {
+          e.target.style.backgroundColor = 'white';
+          document.querySelector('#input-error-password').classList.remove('input-error-password-active')
+          document.querySelector('#input-error-password').classList.add('input-error-password')
+        }else{
+          e.target.style.backgroundColor = 'pink';
+          document.querySelector('#input-error-password').classList.add('input-error-password-active')
+          document.querySelector('#input-error-password').classList.remove('input-error-password')
+        }
+      break;
     }
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
   })
 
+  formulario.addEventListener('submit', (e) => {
 
-  //validaciones password
-  password.addEventListener('blur', (e) => {
-
-  })
-
-  btnSubmit.addEventListener('click', (e) => {
-    if(errores.length > 0){
-      e.preventDefault();
+    e.preventDefault();
       
-    }
-  })   
+  })    
+  
 }
