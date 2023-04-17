848897469eb8e6226a491891217dd96b13c265e5
Emilix22 <torresdragon@hotmail.com>
2023-04-16 21:37:03 -0300

    modifico validaciones front en form login

--- public/js/validationsFrontLogin.js
+++ public/js/validationsFrontLogin.js
@@ -1,47 +1,53 @@
 window.onload = function () {
 
+  //capturo los elementos a validar, pongo las expresiones regulares y los campos para validar en el submit.
   const formulario = document.querySelector('#formulario');
   const inputs = document.querySelectorAll('#formulario .form-camp input');
   const expresiones = {
     email: /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i,
     password: /^.{8,30}$/
     }
+  const campos = {
+    email: false,
+    password: false
+  }  
 
+  //funcion para validar el formulario, un caso para cada input  
   const validarFormulario = (e) => {
+
     switch (e.target.name) {
       case 'email':
-        if (expresiones.email.test(e.target.value)) {
-          e.target.style.backgroundColor = 'white';
-          document.querySelector('#input-error-email').classList.remove('input-error-email-active')
-          document.querySelector('#input-error-email').classList.add('input-error-email')
-        }else{
-          e.target.style.backgroundColor = 'pink';
-          document.querySelector('#input-error-email').classList.add('input-error-email-active')
-          document.querySelector('#input-error-email').classList.remove('input-error-email')
-        }
+        validarCampo(expresiones.email, e.target, "email")
       break;
+
       case 'password':
-        if (expresiones.password.test(e.target.value)) {
-          e.target.style.backgroundColor = 'white';
-          document.querySelector('#input-error-password').classList.remove('input-error-password-active')
-          document.querySelector('#input-error-password').classList.add('input-error-password')
-        }else{
-          e.target.style.backgroundColor = 'pink';
-          document.querySelector('#input-error-password').classList.add('input-error-password-active')
-          document.querySelector('#input-error-password').classList.remove('input-error-password')
-        }
+        validarCampo(expresiones.password, e.target, "password")
       break;
     }
   }
-
+  //funcion para validar cada input
+  const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+      input.style.backgroundColor = 'white';
+      document.querySelector(`#camp-${campo} .input-error`).classList.remove('input-error-active');
+      campos[campo] = true;
+    }else{
+      input.style.backgroundColor = 'pink';
+      document.querySelector(`#camp-${campo} .input-error`).classList.add('input-error-active');
+      campos[campo] = false;
+    }
+  }
+  //agrego los eventos a cada input
   inputs.forEach((input) => {
     input.addEventListener('keyup', validarFormulario);
     input.addEventListener('blur', validarFormulario);
   })
-
+  //evento submit del formulario
   formulario.addEventListener('submit', (e) => {
-
-    e.preventDefault();
+    
+    if (campos.email == false || campos.password == false) {
+      e.preventDefault();
+    }
       
   })    
   
