e0f52f8df2facf5e8bfe77cb528a21408abd580c
Emilix22 <torresdragon@hotmail.com>
2023-04-20 06:13:36 -0300

    genero endpoint para listado de usuarios

--- controllers/apiControllers/usersController.js
+++ controllers/apiControllers/usersController.js
@@ -6,9 +6,21 @@ const usersAPIController = {
 
         .then(users => {
 
+            let datos = [];
+
+            users.forEach(user => {
+
+                datos.push({
+                    id: user.id,
+                    name: user.first_name,
+                    email: user.email,
+                    detail: `http://localhost:3000/api/users/${user.id}`
+                })
+            });
+
             return res.status(200).json({
                 count: users.length,
-                data: users,
+                users: datos,
                 status: 200
             })
         })
